31dda8ee724f7ecd850aca4bb8d827febe9c586c
Jovanko Kenshian <78436437+jovankokenshian@users.noreply.github.com>
2021-10-08 00:13:13 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,45 @@
-import "./App.css";
-import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-	Redirect,
-} from "react-router-dom";
-import Login from "./components/Login";
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import Login from './components/Login';
 import Navbar from "./components/Navbar";
 import Waves from "./components/Waves";
-import WelcomePage from "./components/WelcomePage";
-import Register from "./components/Register";
-import Dashboard from "./components/Dashoard";
-import { useEffect } from "react";
+import WelcomePage from './components/WelcomePage';
+import Register from './components/Register'
+import { useEffect, useState } from 'react'
+import { useAuth } from './context/AuthContext';
+
 
 function App() {
-	useEffect(() => {
-		document.body.className = "bg-gray-100";
-		document.title = "UMNLP";
-	}, []);
-	return (
-		<Router>
-			<Navbar />
-			<Switch>
-				<Route path="/" exact>
-					<WelcomePage />
-				</Route>
-				<Route path="/login">
-					<Login />
-				</Route>
-				<Route path="/register">
-					<Register />
-				</Route>
-				<Route path="/dashboard">
-					<Dashboard />
-				</Route>
-			</Switch>
-		</Router>
-	);
+  useEffect(() => {
+    document.body.className = 'bg-gray-100'
+    document.title = "Trash Sorter";
+  }, []);
+  const [loggedIn, setLoggedIn] = useState()
+  const { checkUserLogin } = useAuth()
+
+  useEffect(() => {
+    async function setLogin() {
+      const data = await checkUserLogin()
+      setLoggedIn(data)
+    }
+    setLogin()
+  }, [checkUserLogin])
+  return (
+    <Router>
+      <Navbar loggedIn={loggedIn} />
+      <Switch>
+        <Route path="/" exact>
+          <WelcomePage />
+        </Route>
+        <Route path="/login">
+          {loggedIn ? <Redirect to="/" exact /> : <Login />}
+        </Route>
+        <Route path="/register">
+          {loggedIn ? <Redirect to="/" exact /> : <Register />}
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
