e7a3a56edff874b23f893fd7ee633d61240331ab
CrystallizedSyrup <jovanko5828@gmail.com>
2021-10-08 00:06:25 +0700

    fungsionalitas login, register, blabla

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import LinkButton from './NavBarComponent/LinkButton'
 import Nav from './NavBarComponent/Nav'
 import MenuTitle from './NavBarComponent/MenuTitle';
-import { useHistory } from "react-router-dom"
 
-export default function Navbar() {
+
+export default function Navbar({ loggedIn }) {
     const [showMenu, setShowMenu] = useState(false)
-    const userName = false
     const [navActive, setNavActive] = useState([false, false])
-    console.log(navActive)
-
-
 
     return (
         <Nav>
@@ -19,10 +15,10 @@ export default function Navbar() {
 
             <div className={`${showMenu ? "flex" : "hidden"} flex-col mt-2 space-y-4 md:flex md:space-y-0 
             md:flex-row md:items-center md:space-x-10 md:mt-0`}>
-                {userName ?
-                    <LinkButton active={navActive[0]} onClick={(() => { setNavActive([true, false]) })} dest="/profile">{userName}</LinkButton> :
+                {loggedIn ?
+                    <LinkButton active={navActive[0]} onClick={(() => { setNavActive([true, false]) })} dest="/profile">Profile</LinkButton> :
                     <LinkButton active={navActive[0]} onClick={(() => { setNavActive([true, false]) })} dest="/register">Register</LinkButton>
-                }{userName ?
+                }{loggedIn ?
                     <LinkButton active={navActive[1]} onClick={(() => { setNavActive([false, true]) })} dest="/logout" >Logout</LinkButton> :
                     <LinkButton active={navActive[1]} onClick={(() => { setNavActive([false, true]) })} dest="/login">Login</LinkButton>
                 }
