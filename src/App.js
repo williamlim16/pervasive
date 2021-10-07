f74bb49c90b0c242300ace7c7d9de7054f3c2410
william <williaml1601@gmail.com>
2021-10-07 20:28:50 +0700

    dashboard card

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
-import './App.css';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import Login from './components/Login';
+import "./App.css";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect,
+} from "react-router-dom";
+import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 import Waves from "./components/Waves";
-import WelcomePage from './components/WelcomePage';
-import Register from './components/Register'
-
+import WelcomePage from "./components/WelcomePage";
+import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/" exact>
-          <WelcomePage />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/register">
-          <Register />
-        </Route>
-      </Switch>
-    </Router>
-  );
+	return (
+		<Router>
+			<Navbar />
+			<Switch>
+				<Route path="/" exact>
+					<WelcomePage />
+				</Route>
+				<Route path="/login">
+					<Login />
+				</Route>
+				<Route path="/register">
+					<Register />
+				</Route>
+				<Route path="/dashboard">
+					<Dashboard />
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
