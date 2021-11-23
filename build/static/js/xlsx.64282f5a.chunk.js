fcdf9269f460bd5768aaa9253228ca992a6f2e17
William <william16.lim@gmail.com>
2021-11-23 16:31:28 +0700

    add build folder

---
+++ build/static/js/xlsx.64282f5a.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonptrash_sorter=this.webpackJsonptrash_sorter||[]).push([[2],{368:function(s,t){},461:function(s,t){},462:function(s,t){}}]);
+//# sourceMappingURL=xlsx.64282f5a.chunk.js.map
